06924f7468ae0a0ff49bff14fbb903e15e680309
devendra-70 <devendramahajan70@gmail.com>
2025-07-22 01:47:08 +0530

    added rssfeed model

---
+++ source-services/src/main/java/com/infocus/source_services/model/RSSFeed.java
@@ -0,0 +1,22 @@
+package com.source.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "rss_feeds", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"region", "category", "url"})
+})
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RssFeed {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String region;
+    private String category;
+    private String url;
+}
