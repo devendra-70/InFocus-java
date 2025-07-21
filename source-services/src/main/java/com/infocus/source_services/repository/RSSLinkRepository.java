199c2ad03cec0fefbb1f64ff5acb2a7227144897
devendra-70 <devendramahajan70@gmail.com>
2025-07-22 01:30:10 +0530

    added news rss link repo

---
+++ source-services/src/main/java/com/infocus/source_services/repository/RSSLinkRepository.java
@@ -0,0 +1,7 @@
+package com.dev.sourceservice.repository;
+
+import com.dev.sourceservice.model.RSSLink;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RSSLinkRepository extends JpaRepository<RSSLink, Long> {
+}
