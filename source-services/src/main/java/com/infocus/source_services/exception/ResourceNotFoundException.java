bdc5ac6b84519504e2288d89f5aa221e602e6779
devendra-70 <devendramahajan70@gmail.com>
2025-07-22 02:11:52 +0530

    updated exception

--- source-services/src/main/java/com/infocus/source_services/exception/ResourceNotFoundException.java
+++ source-services/src/main/java/com/infocus/source_services/exception/ResourceNotFoundException.java
@@ -1,7 +1,19 @@
 package com.infocus.source_services.exception;
 
+/**
+ * Custom exception thrown when a requested resource is not found.
+ */
 public class ResourceNotFoundException extends RuntimeException {
+
     public ResourceNotFoundException(String message) {
         super(message);
+    }
+
+    public ResourceNotFoundException(String resourceName, Long id) {
+        super(String.format("%s with ID %d not found", resourceName, id));
+    }
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s: '%s'", resourceName, fieldName, fieldValue));
     }
 }
