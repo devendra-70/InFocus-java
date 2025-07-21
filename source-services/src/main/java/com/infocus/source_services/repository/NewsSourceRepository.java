fa10c4b85c2b44e51c637d6135fb3c36561aa511
devendra-70 <devendramahajan70@gmail.com>
2025-07-22 01:29:20 +0530

    added news source repo

---
+++ source-services/src/main/java/com/infocus/source_services/repository/NewsSourceRepository.java
@@ -0,0 +1,11 @@
+package com.dev.sourceservice.repository;
+
+import com.dev.sourceservice.model.NewsSource;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface NewsSourceRepository extends JpaRepository<NewsSource, Long> {
+    boolean existsByName(String name);
+    Optional<NewsSource> findByName(String name);
+}
